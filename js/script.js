6eb8509851615080870229ae899903dec7de38e6
Tahmasib Shirinzade <tshzade@gmail.com>
2019-12-09 17:25:27 +0400

    update2

--- js/script.js
+++ js/script.js
@@ -1,42 +1,43 @@
 //navbar toggler
-$(document).ready(function(){/*ancaq doc yuklenende, hazir olanda ready olsun*/
+$(document).ready(function () {/*ancaq doc yuklenende, hazir olanda ready olsun*/
   //toggler button
-$('.navbar-toggler').click(function(){
-  $('.navbar-toggler').toggleClass('change')
-})
+  $('.navbar-toggler').click(function () {
+    $('.navbar-toggler').toggleClass('change')
+  })
 });
- 
- //view more
- $("#toggle-vm").click(function() {
-    var elem = $("#toggle-vm").text();
-    if (elem == "View More") {
-      //Stuff to do when btn is in the read more state
-      $("#toggle-vm").text("View Less");
-      $("#menu-displaynone").slideDown();
-    } else {
-      //Stuff to do when btn is in the read less state
-      $("#toggle-vm").text("View More");
-      $("#menu-displaynone").slideUp();
-    }
-  });
 
-  //dropdown hover
-  function toggleDropdown (e) {
-    const _d = $(e.target).closest('.dropdown'),
-      _m = $('.dropdown-menu', _d);
-    setTimeout(function(){
-      const shouldOpen = e.type !== 'click' && _d.is(':hover');
-      _m.toggleClass('show', shouldOpen);
-      _d.toggleClass('show', shouldOpen);
-      $('[data-toggle="dropdown"]', _d).attr('aria-expanded', shouldOpen);
-    }, e.type === 'mouseleave' ? 100 : 0);
+//view more
+$("#toggle-vm").click(function () {
+  var elem = $("#toggle-vm").text();
+  if (elem == "View More") {
+    //Stuff to do when btn is in the read more state
+    $("#toggle-vm").text("View Less");
+    $("#menu-displaynone").slideDown();
+  } else {
+    //Stuff to do when btn is in the read less state
+    $("#toggle-vm").text("View More");
+    $("#menu-displaynone").hide();
+
   }
-  
-  $('body')
-    .on('mouseenter mouseleave','.dropdown',toggleDropdown)
-    .on('click', '.dropdown-menu a', toggleDropdown);
-  
-  
+});
+
+//dropdown hover
+function toggleDropdown(e) {
+  const _d = $(e.target).closest('.dropdown'),
+    _m = $('.dropdown-menu', _d);
+  setTimeout(function () {
+    const shouldOpen = e.type !== 'click' && _d.is(':hover');
+    _m.toggleClass('show', shouldOpen);
+    _d.toggleClass('show', shouldOpen);
+    $('[data-toggle="dropdown"]', _d).attr('aria-expanded', shouldOpen);
+  }, e.type === 'mouseleave' ? 100 : 0);
+}
+
+$('body')
+  .on('mouseenter mouseleave', '.dropdown', toggleDropdown)
+  .on('click', '.dropdown-menu a', toggleDropdown);
+
+
 //aos animation 
 AOS.init({
   duration: 3000,
