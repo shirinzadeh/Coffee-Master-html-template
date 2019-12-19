04f30a65e7e990b01e0e388003a5a3038845e26b
Tahmasib Shirinzade <tshzade@gmail.com>
2019-12-19 13:57:27 +0400

    blog section was added

--- js/script.js
+++ js/script.js
@@ -65,6 +65,33 @@ $(document).ready(function () {
     }
   });
 
+  $(".carousel-blog").owlCarousel({
+    autoplay: true,
+    autoplayHoverPause: true,
+    margin: 0,
+    stagePadding: 0,
+    nav: true,
+    dots: false,
+    navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      485: {
+        items: 1
+      },
+      728: {
+        items: 3
+      },
+      960: {
+        items: 4
+      },
+      1200: {
+        items: 4
+      }
+    }
+  });
+
   // about team 
   // default state of bio
   var bioActive = false;
