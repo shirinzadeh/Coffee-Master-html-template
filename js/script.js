0d886170f790c2d2d62be21d86ba35c2c4ae606b
Tahmasib Shirinzade <tshzade@gmail.com>
2020-01-04 18:21:07 +0400

    modified magnific popup

--- js/script.js
+++ js/script.js
@@ -184,7 +184,12 @@ $(document).ready(function () {
   $('.gallery-inner').magnificPopup({
     delegate: 'a', // child items selector, by clicking on it popup will open
     type: 'image',
-    gallery: { enabled: true }
+    gallery: { enabled: true },
+    zoom: {
+      enabled: true, // By default it's false, so don't forget to enable it
+      duration: 300, // duration of the effect, in milliseconds
+      easing: 'ease-in-out', // CSS transition easing function
+    }
   });
 
   // gallery filter 
